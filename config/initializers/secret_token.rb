bb0d89293247e6f4e8b99e866548448993004e4d
Dan Nguyen <dairy@Dan-Nguyens-MacBook-Air.local>
2011-04-30 12:53:57 -0400

    First rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SummerMovies::Application.config.secret_token = '9d98fbab7db508b4236809cf2bc5f504c0e70b257bfffed72023adb7de560fbf79e493b445d59672ce0591247f72c5f2f03fee9107ad9fa3af1d5546dfcb7844'
